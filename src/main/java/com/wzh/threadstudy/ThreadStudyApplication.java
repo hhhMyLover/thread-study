c0402f427c1881e1cd75dddb4ff5b5ebf15d57c9
hhhMyLover <2454988619@qq.com>
2025-11-09 17:45:46 +0800

    --init thread-study

---
+++ src/main/java/com/wzh/threadstudy/ThreadStudyApplication.java
@@ -0,0 +1,13 @@
+package com.wzh.threadstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadStudyApplication.class, args);
+    }
+
+}
